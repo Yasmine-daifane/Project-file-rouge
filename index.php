a76d5b4e04ac3e24ad28191be11b40c73acd146a
yasmine <daifaney@gmail.com>
2023-06-12 05:13:31 -0700

    adding the main structure

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 
 
-// 
-// Afficher la liste des projets 
-//
 
-// if request isn't ajax:
 
 ?>
 <!DOCTYPE html>
@@ -13,9 +9,12 @@
 <?php
 include './Views/Layout/head.php';
 ?>
-<?php
-include './Views/Assets/css/index.css';
-?>
+<style>
+    <?php
+    include './Views/Assets/css/index.css';
+    ?>
+</style>
+
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
@@ -31,25 +30,72 @@ include './Views/Assets/css/index.css';
 
         <!-- Main Sidebar Container -->
         <?php
-        include  './Views/Layout/sidebar.php' ;
+        include  './Views/Layout/sidebar.php';
         ?>
         <!-- /.sidebar -->
 
         <!-- Content Wrapper. Contains page content -->
-      <main>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            Welcome admin
+                        </div>
 
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+            <section class="content d-flex flex-column justify-content-center align-items-center ">
+                <!-- Default box -->
+                <div class="row w-100" style="gap: 20px;">
+                    <div class="col-4 like-dislike-container">
+                        <a href="#">
+                            <div class="tool-box">
+                                <hr class="elment">
+                            </div>
+                            <p class="text-content">Department</p>
+                        </a>
+                    </div>
+                    <div class="col-4 like-dislike-container">
+                        <div class="tool-box">
+                            <hr class="elment">
+                        </div>
+                        <p class="text-content">Services</p>
+                    </div>
+                    <div class="col-4 like-dislike-container">
+                        <div class="tool-box">
+                            <hr class="elment">
+                        </div>
+                        <p class="text-content">Requests</p>
+                    </div>
+                    <div class="col-4 like-dislike-container">
+                        <div class="tool-box">
+                            <hr class="elment">
+                        </div>
+                        <p class="text-content">Costumers</p>
+                    </div>
 
-
-      
-      </main>
-        <!-- /.content-wrapper -->
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+                </div>
+        </div>
+        </section>
     </div>
-  
+    <!-- /.content-wrapper -->
+
+
+
+
+
+    <!-- /.content-wrapper -->
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+    </div>
+
     <!-- ./wrapper -->
     <!-- links script -->
     <?php
